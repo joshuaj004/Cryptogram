f08ad884e89197908d73fd9ae7958c3b6837d908
Josh <joshuaj004@gmail.com>
2017-12-25 13:32:56 -0800

    First Iteration
    This is a simple cryptogram website that pulls in a cryptogram quote
    from a lambda function

---
+++ index.js
@@ -0,0 +1,108 @@
+console.log("We're live here");
+QUOTES_URL1 = 'https://talaikis.com/api/quotes/random/'
+QUOTES_URL = 'http://localhost:5000/'
+LAMBDA_URL = "https://qk5fq1j6i8.execute-api.us-west-2.amazonaws.com/production"
+uppercaseASCII = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+function createLetterButtons() {
+    for (var c = 0; c < uppercaseASCII.length; c++) {
+        var divWrapper = document.createElement("div");
+        divWrapper.className = "input-group";
+        divWrapper.className += " characterInput";
+        var innerSpan = document.createElement("span");
+        innerSpan.className = "input-group-addon";
+        innerSpan.innerHTML = uppercaseASCII[c];
+        var innerInput = document.createElement("input");
+        innerInput.class = "form-control";
+        innerInput.type = "text";
+        innerInput.setAttribute("maxlength", "1");
+        innerInput.onchange = letterButtonOnClick(uppercaseASCII[c]);
+        innerInput.id = uppercaseASCII[c];
+        divWrapper.appendChild(innerSpan);
+        divWrapper.appendChild(innerInput);
+        document.getElementById("characterInputs").appendChild(divWrapper);
+    }
+}
+createLetterButtons();
+
+
+function letterButtonOnClick(origLetter) {
+    return function() {
+        var newValue = document.getElementById(origLetter).value;
+        var quoteIndices = getQuoteIndices(origLetter);
+        var authorIndices = getAuthorIndices(origLetter);
+        if (newValue == "") {
+            newValue = "*";
+        }
+        newValue = newValue.toUpperCase();
+        var decryptedQuote = document.getElementById("decryptedQuote").innerHTML;
+        var decryptedAuthor = document.getElementById("decryptedAuthor").innerHTML;
+        for (var i = 0; i < quoteIndices.length; i++) {
+            decryptedQuote = replaceString(decryptedQuote, newValue, quoteIndices[i]);
+        }
+        for (var i = 0; i < authorIndices.length; i++) {
+            decryptedAuthor = replaceString(decryptedAuthor, newValue, authorIndices[i]);
+        }
+        document.getElementById("decryptedQuote").innerHTML = decryptedQuote;
+        document.getElementById("decryptedAuthor").innerHTML = decryptedAuthor;
+    }
+}
+
+
+function replaceString(string, char, index) {
+    var s = string.substr(0, index) + char + string.substr(index + 1);
+    return s;
+}
+
+
+function getAuthorIndices(origLetter) {
+    var indices = [];
+    var encryptedAuthor = response["author"];
+    for (var i = 0; i < encryptedAuthor.length; i++) {
+        if (encryptedAuthor[i] == origLetter) {
+            indices.push(i);
+        }
+    }
+    return indices;
+}
+
+
+function getQuoteIndices(origLetter) {
+    var indices = [];
+    var encryptedQuote = response["quote"];
+    for (var i = 0; i < encryptedQuote.length; i++) {
+        if (encryptedQuote[i] == origLetter) {
+            indices.push(i);
+        }
+    }
+    return indices;
+}
+
+
+function processQuote() {
+    document.getElementById("encryptedQuote").innerHTML = response["quote"];
+    document.getElementById("encryptedAuthor").innerHTML = response["author"];
+    formatDecryptedQuote(response["quote"], response["author"]);
+}
+
+
+function getQuote() {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            response = JSON.parse(xmlHttp.responseText)
+            processQuote();
+        }
+    }
+    xmlHttp.open("GET", LAMBDA_URL, true); // true for asynchronous
+    xmlHttp.send(null);
+
+}
+
+
+function formatDecryptedQuote(quote, author) {
+    var newQuote = quote.replace(/[a-zA-Z]/g, "*");
+    var newAuthor = author.replace(/[a-zA-Z]/g, "*");
+    document.getElementById("decryptedQuote").innerHTML = newQuote;
+    document.getElementById("decryptedAuthor").innerHTML = newAuthor;
+}
---
+++ quoteGrabber.py
@@ -0,0 +1,83 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+import requests, json, re, string, random, html
+__author__ = 'Josh'
+# QUOTES_URL = 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1'
+QUOTES_URL = 'https://talaikis.com/api/quotes/random/'
+
+
+def main():
+    quoteInfo = getQuote()
+    strippedQuote = stripQuote(html.unescape(quoteInfo['quote']))
+    shiftsDict = generateShifts()
+    shiftedQuote = shiftQuote(strippedQuote, shiftsDict)
+    shiftedAuthor = shiftAuthor(quoteInfo['author'].upper(), shiftsDict)
+    print(shiftedQuote)
+    print(shiftedAuthor)
+    input("Solution?")
+    print(strippedQuote)
+    print(quoteInfo['author'].upper())
+
+
+@app.route('/')
+def api():
+    quoteInfo = getQuote()
+    strippedQuote = stripQuote(html.unescape(quoteInfo['quote']))
+    print(strippedQuote)
+    print(quoteInfo['author'].upper())
+    shiftsDict = generateShifts()
+    shiftedQuote = shiftQuote(strippedQuote, shiftsDict)
+    shiftedAuthor = shiftAuthor(quoteInfo['author'].upper(), shiftsDict)
+    quoteDict = {}
+    quoteDict["author"] = shiftedAuthor
+    quoteDict["quote"] = shiftedQuote
+    response = jsonify(quoteDict)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+
+def shiftString(string, shiftsDict):
+    shiftedString = ""
+    for c in string:
+        if c in shiftsDict:
+            shiftedString += shiftsDict[c]
+        else:
+            shiftedString += c
+    return shiftedString
+
+
+def shiftAuthor(author, shiftsDict):
+    return shiftString(author, shiftsDict)
+
+
+def shiftQuote(quote, shiftsDict):
+    return shiftString(quote, shiftsDict)
+
+
+def generateShifts():
+    shiftsDict = {}
+    outputList = list(string.ascii_uppercase)
+    random.shuffle(outputList)
+    for letter in string.ascii_uppercase:
+        randomLetter = outputList.pop()
+        while letter == randomLetter:
+            outputList.append(randomLetter)
+            random.shuffle(outputList)
+            randomLetter = outputList.pop()
+        shiftsDict[letter] = randomLetter
+    return shiftsDict
+
+
+def stripQuote(quote):
+    return re.sub('<[^<]+?>', '', quote).strip().upper()
+
+def getQuote():
+    r = requests.get(QUOTES_URL)
+    return json.loads(r.text)
+
+
+if __name__ == '__main__':
+    main()
+
